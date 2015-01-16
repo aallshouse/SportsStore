f25d38af46b3115e54363cb791609c77d83beb28
aallshouse <aallshouse@matrixformedia.com>
2015-01-16 13:51:22 -0500

    Initial Commit

---
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+
+namespace SportsStore.UnitTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void Can_Paginate()
+    {
+      // Arrange
+      var mock = new Mock<IProductRepository>();
+      mock.Setup(m => m.Products).Returns(new Product[]
+      {
+        new Product {ProductId = 1, Name = "P1"},
+        new Product {ProductId = 2, Name = "P2"},
+        new Product {ProductId = 3, Name = "P3"},
+        new Product {ProductId = 4, Name = "P4"},
+        new Product {ProductId = 5, Name = "P5"}
+      });
+      var controller = new ProductController(mock.Object) {PageSize = 3};
+      
+      try
+      {
+        // Act
+        var result = (IEnumerable<Product>) controller.List(2).Model;
+        // Assert
+        var prodArray = result.ToArray();
+        Assert.IsTrue(prodArray.Length == 2);
+        Assert.AreEqual(prodArray[0].Name, "P4");
+        Assert.AreEqual(prodArray[1].Name, "P5");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex);
+      }
+    }
+  }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+  public class ProductController : Controller
+  {
+    private readonly IProductRepository _productRepository;
+    public int PageSize = 4;
+
+    public ProductController(IProductRepository productRepository)
+    {
+      _productRepository = productRepository;
+    }
+
+    public ViewResult List(int page = 1)
+    {
+      return View(_productRepository.Products
+        .OrderBy(p => p.ProductId)
+        .Skip((page - 1) * PageSize)
+        .Take(PageSize)
+        );
+    }
+  }
+}
